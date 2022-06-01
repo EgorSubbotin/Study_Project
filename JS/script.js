0735f0ebce5ed105f9d82d02086145e14da9b40e
Egor <subbotinegor2590@gmail.com>
2022-06-01 19:38:10 +0200

    first co;;it

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+//const result=confirm('Are you here');
+//console.log(result);
+const arr =[];
+
+arr[0]=prompt('Quel votr prenome', '');
+arr[1]=prompt('Quel votr nome', '');
+arr[2]=prompt('Quel age', ``);
+
+console.log(`${arr} /5`);
