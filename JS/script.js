db31259f31b45510db377cf9616527ae08834969
Egor <subbotinegor2590@gmail.com>
2022-06-06 19:13:31 +0200

    ecrir pPiramida

--- JS/script.js
+++ JS/script.js
@@ -17,3 +17,29 @@ const lMovieDB= {
 lMovieDB.movies[a]=b;
 lMovieDB.movies[c]=d;
 console.log(lMovieDB.movies);
+
+
+if ('hgkh'){
+    console.log(true);
+}
+
+let q=3, t=0, s=3, e=2;
+console.log(q===3 && (t===2 || e) && s===3);
+
+console.log(5===5&& 3>1 || 5);
+
+
+// let result='';
+// for (let i=0; i<6; i++){
+//        result+='*';
+//       console.log(result);
+// }
+
+let res='';
+for(let i=0; i<7; i++){
+    for (let j=0; j <i; j++){
+        res+='*';
+    }
+    res+='\n';
+}
+console.log(res);
