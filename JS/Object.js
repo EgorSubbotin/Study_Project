dde263c19f48874569ddc44d4cabd9809efd1e71
Egor <subbotinegor2590@gmail.com>
2022-06-21 14:17:11 +0200

    Клонироваиние. Создание поверхностных копий

--- JS/Object.js
+++ JS/Object.js
@@ -1,23 +1,103 @@
 "use strict";
 
+// const options = {
+// name: 'test',
+// widht: 1024,
+// height: 1024,
+// colors:{
+//     border:'black',
+//     br:'red'
+// }
+// };
+
+// const coppyOptions = copy(options);
+// //const coppyOptions={...options};
+// coppyOptions.widht=100;
+// coppyOptions.colors.br ='rouge';
+// console.log(coppyOptions, options);
+
+// function copy(nom){ 
+//     const objCopy={};
+//     for (let key in nom){
+//         if (typeof(nom[key]) === 'object'){
+//             objCopy[key]={...nom[key]};
+//         } else {
+//            objCopy[key]=nom[key];                 
+//         }                
+//     }
+//     return objCopy;
+// }
+
+
+// function copy(nom){ 
+//     const objCopy={};
+//     for (let key in nom){
+//         objCopy[key]=nom[key];       
+//     }
+//     return objCopy;
+// }
+
+
+// for (let key in options){
+//     if (typeof(options[key]) === 'object'){
+//         for (let i in options[key]){
+//             console.log(`свойства ${i} со значением ${options[key][i]}`);
+//         }
+//     } else {
+//         console.log(`свойства ${key} со значением ${options[key]}`);
+//     }       
+// } 
+
+//console.log(Object.keys(options).length);
+
+
 const options = {
-name: 'test',
-widht: 1024,
-height: 1024,
-colors:{
-    border:'black',
-    br:'red'
-}
-};
-
-for (let key in options){
-    if (typeof(options[key]) === 'object'){
-        for (let i in options[key]){
-            console.log(`свойства ${i} со значением ${options[key][i]}`);
+    name: 'test',
+    widht: 1024,
+    height: 1024,
+    colors:{
+        border:'black',
+        br:'red',
+        tr: {
+            aw:1,
+            bw:2
         }
-    } else {
-        console.log(`свойства ${key} со значением ${options[key]}`);
-    }       
-}
+    }
+};
+    const coppyOptions = copy(options);
+    //const coppyOptions={...options};
+    coppyOptions.widht=100;
+    coppyOptions.colors.br ='rouge';
+    coppyOptions.colors.tr.aw = 55;
+    console.log(coppyOptions, options);
+    
+    
+    // function copy(nom){ 
+    //     const objCopy={};
+    //     for (let key in nom){
+    //         if (typeof(nom[key]) === 'object'){                
+    //             objCopy[key]={...nom[key]};               
+    //         } else {
+    //            objCopy[key]=nom[key];                 
+    //         }                
+    //     }
+    //     return objCopy;
+    // }
 
-console.log(Object.keys(options).length);
+    function copy(nom){ 
+        const objCopy={};
+        for (let key in nom){
+            if (typeof(nom[key]) === 'object'){
+                for (let key1 in nom[key]){
+                    if (typeof(nom[key][key1]) === 'object'){
+                        objCopy[key][key1]={...nom[key][key1]};
+                    }else {
+                        objCopy[key]={...nom[key]};
+                    }
+                }                
+            } else {
+               objCopy[key]=nom[key];                 
+            }                
+        }
+        return objCopy;
+    }
